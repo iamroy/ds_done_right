624324f5f71395520409fd917503540d0a7757cd
iamroy <saibal1986roy@yahoo.com>
2022-03-25 23:33:55 -0400

    Added reverse linked list 1 and 2 (206 and 92)
    Reverse linked list solution requires two passes over the linked list

--- linked_list/ll_problems.py
+++ linked_list/ll_problems.py
@@ -1,10 +1,115 @@
+#206. Reverse Linked List
+#92. Reverse Linked List II
 #203. Remove Linked List Elements
 #234. Palindrome Linked List
 #83. Remove Duplicates from Sorted List
+#1836. Remove Duplicates From an Unsorted Linked List
 #141. Linked List Cycle
+
 
 from linked_list.ll_node import Node
 from collections import defaultdict
+
+#206. Reverse Linked List
+#Input: head = [1,2,3,4,5]
+#Output: [5,4,3,2,1]
+def reverse_list(linked_list):
+
+    head_node = linked_list.head
+    if not head_node:
+        return
+
+    if head_node.next is None:
+        return head_node
+
+    curr_node = head_node
+    prev_node = None
+
+    while curr_node.next:
+        next_node = curr_node.next
+        curr_node.next = prev_node
+        prev_node = curr_node
+        curr_node = next_node
+
+    curr_node.next = prev_node
+    linked_list.head = curr_node
+    return linked_list
+
+#92. Reverse Linked List II
+#Input: head = [1,2,3,4,5], left = 2, right = 4
+#Output: [1,4,3,2,5]
+#2 pass algorithm
+def reverse_between(linked_list, left, right):
+
+    head_node = linked_list.head
+    curr_node = head_node
+    counter = 0
+    node_val = []
+
+    while curr_node:
+        counter += 1
+        if counter >= left and counter <= right:
+            node_val.append(curr_node.data)
+        curr_node = curr_node.next
+
+    curr_node = head_node
+    counter = 0
+
+    while curr_node:
+        counter += 1
+        if counter >= left and counter <= right:
+            curr_node.data = node_val.pop()
+        curr_node = curr_node.next
+
+    return linked_list
+
+
+#1 pass algorithm
+def reverse_between_one_pass(linked_list, left, right):
+    return linked_list
+
+    """
+    head_node = linked_list.head
+    curr_node = head_node
+    prev_node = None
+    rev_tail_node= None
+    rev_head_node = None
+    counter = 0
+    no_rev_left_end_node = None
+    no_rev_right_start_node = None
+
+    while curr_node:
+        counter += 1
+        if counter>=left or counter <=right and curr_node.next:
+
+            if counter == left:
+                rev_tail_node = curr_node
+            if counter == right:
+                rev_head_node = curr_node
+
+            next_node = curr_node.next
+            curr_node.next = prev_node
+            prev_node = curr_node
+            curr_node = next_node
+        else:
+            if counter>right:
+                no_rev_right_start_node
+                break
+
+            if counter<left:
+                no_rev_left_end_node = curr_node
+
+            prev_node = curr_node
+            curr_node = curr_node.next
+
+    if not no_rev_left_end_node:
+        no_rev_left_end_node.next = rev_head_node
+
+    if not rev_tail_node:
+        rev_tail_node.next = no_rev_right_start_node
+
+    """
+
 
 #203. Remove Linked List Elements
 def remove_elements(linked_list, val):
@@ -115,6 +220,7 @@ def linked_list_has_cycle(linked_list):
 
     return False
 
+#141. Linked List Cycle
 # two pointer approach
 def linked_list_has_cycle2(linked_list):
     head_node = linked_list.head
@@ -132,3 +238,33 @@ def linked_list_has_cycle2(linked_list):
         fast_ptr = fast_ptr.next.next
 
     return True
+
+#1019. Next Greater Node In Linked List
+#Input: head = [2,7,4,3,5]
+#Output: [7,0,5,5,0]
+def next_larger_nodes(linked_list):
+    pass
+
+#2181. Merge Nodes in Between Zeros
+#Input: head = [0,3,1,0,4,5,2,0]
+#Output: [4,11]
+def merge_nodes(linked_list):
+    pass
+
+#445. Add Two Numbers II
+#Input: l1 = [7,2,4,3], l2 = [5,6,4]
+#Output: [7,8,0,7]
+def add_two_numbers(linked_list1, linked_list12):
+    pass
+
+
+#142. Linked List Cycle II
+#Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+def detect_cycle(linked_list):
+    pass
+
+#369. Plus One Linked List
+#Input: head = [1,2,3]
+#Output: [1,2,4]
+def plus_one(linked_list):
+    pass
